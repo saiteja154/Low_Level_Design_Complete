e39e3eb98b5df5576af619dfde735a57f8509ff9
saiteja154 <rapellisaiteja154@gmail.com>
2024-06-29 14:43:05 +0530

    Synchronization using Synchronized and sync Methods

---
+++ src/main/java/org/example/AdderAndSubtractorSyncMethods/Subtractor.java
@@ -0,0 +1,20 @@
+package org.example.AdderAndSubtractorSyncMethods;
+
+
+import java.util.concurrent.Callable;
+
+public class Subtractor implements Callable<Void> {
+    private Count count;
+
+    Subtractor(Count count){
+        this.count = count;
+    }
+
+    @Override
+    public  Void call() throws Exception {
+        for(int i = 1 ; i <= 10000; i++){
+                count.decrementByX(i);
+        }
+        return null;
+    }
+}
